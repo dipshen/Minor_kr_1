1d1434c18006c26f18d78a443e93a17c84394b64
dipshen <di.pshennikova@gmail.com>
2023-11-08 10:32:12 +0300

    init

---
+++ passport/js.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".block4").click(function() {
+ 		 $(".block4").addClass("none");
+     $(".block4_1").removeClass("none");
+	});
+  $(".block5").click(function() {
+ 		 $(".block5").addClass("none");
+     $(".block5_1").removeClass("none");
+	});
+  $(".block6").click(function() {
+ 		 $(".block6").addClass("none");
+     $(".block6_1").removeClass("none");
+	});
+});
